7e744cce663367b228d72cda5af54340d939fba4
Berkay Erdoğan <quasrawonder1@gmail.com>
2025-04-22 01:23:55 +0300

    Static files uploaded

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn =function(){
-    console.log("Function Working");
-};
+const express = require("express");
+
+const app = express();
+
+const path = require("path");
+
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,"temp/index.html"))
+})
+
+
+const port =3000;
+app.listen(port,()=>{
+    console.log(`Sunucu ${port} portunda başlatıldı..`);
+});
